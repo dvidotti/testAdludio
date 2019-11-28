58e15943c4d031133278656ecb99764a3a012214
dvidotti <danilovidotti@gmail>
2019-11-28 19:21:18 +0000

    refact:frontend alignments and adpting for deskotp screens

--- src/FormBox.jsx
+++ src/FormBox.jsx
@@ -4,15 +4,20 @@ const FormBox = () => {
   return (
       <div className="FormBox">
         <section className="form-box-one">
-
+          <section className="form-box-one--text">
+            <div className="section-one--text">
+              <h1 className="section-one--title">REDIFINING MOBILE ADVERTISING</h1>
+            </div>
+            <div>
+              <p className="section-one--paragraphs" >Please use this form to register</p>
+              <p className="section-one--paragraphs">If you are a member, please <a className="link">login</a></p>
+            </div>
+            </section>
         </section>
-        {/* <picture className="form-box-image">
-          <img className="form-image" src='/asset1.jpg' alt=""/>
-        </picture> */}
         <section className="form-box-second">
           
           <form className="form-box">
-            <img src="./asset3.png"></img>
+            <img className="image-logo" src="./asset3.png"></img>
             <h1 className="login-title">Register</h1>
             <div className="box-input">
               <h3 className="label-floating">Full Name</h3>
@@ -24,9 +29,15 @@ const FormBox = () => {
             </div>
             <div className="box-input">
               <h3 className="label-floating">Password</h3>
-              <input className="input-login" id="full-name" type="text"/>
-              <button className="form-button">REGISTER</button>
+              <input className="input-login" id="full-name" type="password"/>
             </div>
+            <button className="btn-login">REGISTER</button>
+            <button className="btn-login--google">
+              <img className="btn-icon" src="/google_icon.png" alt=""/>
+              <div className="btn-box--text">
+                <h3 className="btn-text" >Sign in with Google</h3>
+              </div>
+              </button>
           </form>   
         </section>
       </div>
